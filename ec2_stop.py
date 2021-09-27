1f2291552a2e50318a20898430cd6ebf55682968
Shubham Sinha <shubham3196@gmail.com>
2021-09-28 00:50:25 +0530

    AWS Lambda Scripts

---
+++ ec2_stop.py
@@ -0,0 +1,15 @@
+import boto3
+region = 'ap-south-1' 
+instances = [] # Provide Instace ID's to be scheduled
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+    
+    client = boto3.client('sns')
+    response = client.publish(
+        TopicArn='arn:aws:sns:ap-south-1:309468567045:mySlackNotifications', # Create the ARN for Push Notification Link - https://docs.aws.amazon.com/sns/latest/dg/sns-create-topic.html
+        Message='All the EC2 on demand instances that are scheduled by AWS Lambda have been stopped',  # Custom Messages
+        Subject='LeanApps AWS servers have been stopped' # Custom Subject
+    )
+    
